7cfebe3bcd43037c0ee9475615b673081f66097c
TECH7Fox <TECH7Fox@users.noreply.github.com>
2024-10-02 00:37:44 +0000

    deploy: 2e5f342447077cfc8c09dfcff9907a35c429947b

---
+++ assets/js/acecf23e.57636759.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfood_blog=self.webpackChunkfood_blog||[]).push([[903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/schrans/","blogTitle":"Blog","authorsListPath":"/schrans/authors"}')}}]);
