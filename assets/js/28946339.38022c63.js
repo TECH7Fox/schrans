ac77f4dde1a590b344559e1f3f64258e560849da
TECH7Fox <TECH7Fox@users.noreply.github.com>
2024-12-29 02:31:34 +0000

    deploy: 53910379142ba203b99c25c9d194bb1dbbddb9f0

---
+++ assets/js/28946339.38022c63.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfood_blog=self.webpackChunkfood_blog||[]).push([[1171],{4057:t=>{t.exports=JSON.parse('{"tag":{"label":"tito","permalink":"/tags/tito","allTagsPath":"/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/tags/tito","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
